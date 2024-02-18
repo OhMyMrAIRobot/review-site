803cef935e385436c52cffc1fe8190a72ac22020
MrAIRobot <gfhieemobilelegends@gmail.com>
2024-02-18 15:05:51 +0300

    categories in admin panel

---
+++ components/sidebarAdmin.php
@@ -0,0 +1,9 @@
+<div class = "admin-left_cont">
+    <h3 class = "admin_left_header">Админ панель</h3>
+    <ul>
+        <li><a class = "admin_sidebar_item" href = "#">Магазины</a></li>
+        <li><a class = "admin_sidebar_item" href = "#">Категории</a></li>
+        <li><a class = "admin_sidebar_item" href = "#">Отзывы</a></li>
+        <li><a class = "admin_sidebar_item" href = "#">Пользователи</a></li>
+    </ul>
+</div>
