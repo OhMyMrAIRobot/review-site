deec8e8a5456ad93a4f62421ac188474b65856de
MrAIRobot <gfhieemobilelegends@gmail.com>
2024-02-18 15:20:42 +0300

    users in admin panel

--- admin/users/index.php
+++ admin/users/index.php
@@ -10,7 +10,7 @@
     <link rel = "stylesheet" href = "../../styles/header.css">
     <link rel = "stylesheet" href = "../../styles/footer.css">
     <link rel = "stylesheet" href = "../../styles/admin.css">
-    <link rel = "stylesheet" href = "./adminCategory.css">
+    <link rel = "stylesheet" href = "./adminUsers.css">
 
     <!--boostrap-->
     <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">-->
@@ -39,39 +39,45 @@
         </div>
 
         <div class = "admin_container_header">
-            <h2>Управление категориями</h2>
+            <h2>Управление пользователями</h2>
             <div class = "admin_table_header">
                 <div class="admin_table_id">ID</div>
-                <div class = "admin_table_name bold">Название</div>
+                <div class = "admin_table_login bold">Логин</div>
+                <div class = "admin_table_email bold">Email</div>
+                <div class = "admin_table_role bold">Роль</div>
                 <div class = "admin_table_control bold">Управление</div>
             </div>
         </div>
 
-        <!--Category 1-->
+        <!--USER 1-->
         <div class = "admin_table_header">
             <div class="admin_table_id">1</div>
-            <div class = "admin_table_name">Категория 1</div>
+            <div class = "admin_table_login">login</div>
+            <div class = "admin_table_email">Email@gmail.com</div>
+            <div class = "admin_table_role">User</div>
             <div class = "admin_table_control">
                 <a class = "admin_table_edit" href = "#">edit</a>
                 <a class = "admin_table_delete" href = "#">delete</a>
             </div>
         </div>
-        <!--Category 1-->
+        <!--USER 1-->
 
-        <!--Category 2-->
+        <!--USER 2-->
         <div class = "admin_table_header">
             <div class="admin_table_id">2</div>
-            <div class = "admin_table_name">Категория 2</div>
+            <div class = "admin_table_login">Admin</div>
+            <div class = "admin_table_email">admin@gmail.com</div>
+            <div class = "admin_table_role">Admin</div>
             <div class = "admin_table_control">
                 <a class = "admin_table_edit" href = "#">edit</a>
                 <a class = "admin_table_delete" href = "#">delete</a>
             </div>
         </div>
-        <!--Category 2-->
+        <!--USER 2-->
+
 
     </div>
 </main>
-
 
 <!--FOOTER-->
 <?php require "../../components/footer.php"?>
