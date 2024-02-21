f2cac97482c525741d29bced42dc99944fd6c051
MrAIRobot <gfhieemobilelegends@gmail.com>
2024-02-21 20:00:05 +0300

    laravel

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/review-site',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '83e314d7ab44e1bc5e504fe80065d5f665b1c707',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/review-site' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '83e314d7ab44e1bc5e504fe80065d5f665b1c707',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
