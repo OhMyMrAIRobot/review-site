9381998dc01f3fc56adb72d56d31b542d4091885
MrAIRobot <gfhieemobilelegends@gmail.com>
2024-04-04 22:29:57 +0300

    header tailwind

--- review-site/resources/views/components/header.blade.php
+++ review-site/resources/views/components/header.blade.php
@@ -1,46 +1,46 @@
 <header class = "header">
-    <div class = "navbar">
-        <h1 class = "title">
-            <a href = "{{route('main.index')}}">My Title</a>
-        </h1>
-
-        <nav class = "navigation">
-            <ul>
-                <li><a class = "nav-btn" href = "{{route('main.index')}}">Главная</a></li>
-{{--                <li><a class = "nav-btn" href = "{{route('main.index', 'shops=all')}}">Магазины</a></li>--}}
-                <li><a class = "nav-btn" href = "{{route('main.index')}}">О нас</a></li>
-                <li>
-                    @if(\Illuminate\Support\Facades\Auth::check())
-                        <a class = "nav-btn">
-                            <i class = "fa fa-user"></i>
-                            @lang(\Illuminate\Support\Facades\Auth::user()->getUsername())
+        <nav class = "bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+            <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                <a href = "{{route('main.index')}}" class="flex items-center">
+{{--                    <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Logo" />--}}
+                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Reviews</span>
+                </a>
+                <div class="flex items-center lg:order-2">
+                    @if(!\Illuminate\Support\Facades\Auth::check())
+                        <a href = "{{route("auth.index")}}" class="text-white hover:bg-gray-700 text-base rounded-lg px-4 py-2 focus:ring-4 mr-2">Log In</a>
+                        <a href = "{{route('register.index')}}" class="text-white text-base focus:ring-primary-300 bg-primary-700 hover:bg-primary-800 focus:ring-4 rounded-lg py-2 px-4 mr-2">Sign Up</a>
+                    @else
+                        <a class="text-white mr-2 text-xl cursor-pointer px-4 py-2 focus:ring-4"
+                            ><i class = "fa fa-user mr-1"></i>@lang(\Illuminate\Support\Facades\Auth::user()->getUsername())
                         </a>
-
-                        <ul class = "cabinet-btns">
-                            @if(\Illuminate\Support\Facades\Auth::user()->isAdmin())
-                                <li><a class = "nav-btn" href = "{{route('shops.index')}}">Админ панель</a></li>
-                            @endif
-
-                            <li>
-                                <form action="{{route('auth.logout')}}" method='POST'>
-                                    @csrf
-                                    <button type='SUBMIT' class = "nav-btn btn">Выход</button>
-                                </form>
-                            </li>
-                        </ul>
-
-                        @else
-                        <a class = "nav-btn" href ={{route('auth.index')}}>
-                            Войти
+                        <a class = "text-gray-300 hover:text-white mr-2 rounded-lg py-1 px-4 border border-gray-400">
+                            <form action="{{route('auth.logout')}}" method='POST'>
+                                @csrf
+                                <button type='submit'>Logout</button>
+                            </form>
                         </a>
-
-                        <ul class = "cabinet-btns">
-                            <li><a class = "nav-btn" href = "{{route('register.index')}}">Регистрация</a></li>
-                        </ul>
                     @endif
+                </div>
 
-                </li>
-            </ul>
+                <div class = "hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1">
+                    <ul class="flex flex-col mt-4 text-xl lg:flex-row lg:space-x-8 lg:mt-0">
+                        <li>
+                            <a href = "{{route('main.index')}}" class="block text-white py-2 rounded hover:text-white">Главная</a>
+                        </li>
+                        <li>
+                            <a href = "{{route('main.index')}}" class="block text-gray-500 py-2 rounded hover:text-white">О нас</a>
+                        </li>
+                        @if(\Illuminate\Support\Facades\Auth::check())
+                            @if(\Illuminate\Support\Facades\Auth::user()->isAdmin())
+                                <li>
+                                    <a href = "{{route('shops.index')}}" class="block text-gray-500 py-2 rounded hover:text-white">Админ панель</a>
+                                </li>
+                            @endif
+                        @endif
+                    </ul>
+                </div>
+
+            </div>
+
         </nav>
-    </div>
 </header>
