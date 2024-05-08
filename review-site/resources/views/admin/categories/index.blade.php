809cbbfff45c066107e69080c7c1004a7ddfc061
MrAIRobot <gfhieemobilelegends@gmail.com>
2024-05-09 01:25:33 +0300

    refactor admin folders

--- review-site/resources/views/admin/categories/index.blade.php
+++ review-site/resources/views/admin/categories/index.blade.php
@@ -5,30 +5,31 @@
 @endsection
 
 @section('rightSide')
-        @include('components.adminCategory.searchBar')
+    @include('components.admin.adminCategories.searchBar')
 
-        <div class="border-b border-r p-4 bg-gray-50 flex gap-x-8">
-            <a href = "{{route('categories.create')}}" class="border rounded-xl bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white font-bold text-base px-6 py-2"
-            >@lang('admin/categories.create')</a>
-        </div>
+    <div class="border-b border-r p-4 bg-gray-50 flex gap-x-8">
+        <a href="{{route('categories.create')}}"
+           class="border rounded-xl bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white font-bold text-base px-6 py-2"
+        >@lang('admin/categories.create')</a>
+    </div>
 
-        @include('components.admin.status')
+    @include('components.admin.status')
 
-        <div class="grid grid-cols-12 mt-1 text-base border-b bg-gray-50">
-            <div class="col-span-1 font-bold pl-3 pt-1 pb-1 border-b">Id</div>
+    <div class="grid grid-cols-12 mt-1 text-base border-b bg-gray-50">
+        <div class="col-span-1 font-bold pl-3 pt-1 pb-1 border-b">Id</div>
 
-            <div class="col-span-5 font-bold pl-3 pt-1 pb-1 border-b">@lang('admin/categories.category')</div>
+        <div class="col-span-5 font-bold pl-3 pt-1 pb-1 border-b">@lang('admin/categories.category')</div>
 
-            <div class="col-span-3 font-bold pl-3 pt-1 pb-1 border-b">@lang('admin/categories.created')</div>
+        <div class="col-span-3 font-bold pl-3 pt-1 pb-1 border-b">@lang('admin/categories.created')</div>
 
-            <div class="col-span-3 font-bold pl-3 pt-1 pb-1 border-b">@lang('admin/categories.control')</div>
+        <div class="col-span-3 font-bold pl-3 pt-1 pb-1 border-b">@lang('admin/categories.control')</div>
 
-            @foreach($categories as $key => $category)
-                @include('components.adminCategory.categoryItem')
-            @endforeach
-        </div>
+        @foreach($categories as $key => $category)
+            @include('components.admin.adminCategories.categoryItem')
+        @endforeach
+    </div>
 
-        <div class = "pagination_main" style="margin-top: 30px">
-            {{ $categories->onEachSide(10)->links('components.pagination') }}
-        </div>
+    <div class="pagination_main" style="margin-top: 30px">
+        {{ $categories->onEachSide(10)->links('components.pagination') }}
+    </div>
 @endsection
--- review-site/resources/views/admin/shops/index.blade.php
+++ review-site/resources/views/admin/shops/index.blade.php
@@ -5,10 +5,11 @@
 @endsection
 
 @section('rightSide')
-    @include('components.adminShop.searchBar')
+    @include('components.admin.adminShops.searchBar')
 
     <div class="border-b border-r p-4 bg-gray-50 flex gap-x-8">
-        <a href = "{{route('shops.create')}}" class="border rounded-xl bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white font-bold text-base px-6 py-2"
+        <a href="{{route('shops.create')}}"
+           class="border rounded-xl bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white font-bold text-base px-6 py-2"
         >@lang('admin/shops.create')</a>
     </div>
 
@@ -26,10 +27,10 @@
         <div class="col-span-3 font-bold pl-3 pt-1 pb-1 border-b overflow-x-hidden">@lang('admin/shops.control')</div>
 
         @foreach($shops as $key => $shop)
-            @include('components.adminShop.shopItem')
+            @include('components.admin.adminShops.shopItem')
         @endforeach
     </div>
-    <div class = "pagination_main" style="margin-top: 30px">
+    <div class="pagination_main" style="margin-top: 30px">
         {{ $shops->onEachSide(10)->links('components.pagination') }}
     </div>
 @endsection
